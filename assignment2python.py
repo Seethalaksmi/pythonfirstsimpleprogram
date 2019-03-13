b53e220624ddc5015477fcc244bda0339898542e
Seethalaksmi <seetha.laksmi@yahoo.com>
2019-03-13 23:17:18 +0100

    assignment2python methods

---
+++ assignment2python.py
@@ -0,0 +1,25 @@
+number = int(input("enter number to be multiplied with 3: "))
+a = int(input("enter number1: "))
+b = int(input("enter number2 to add with number1:"))
+
+#Multiply a number with 3
+def mult(num): 
+
+	multiplication = 3 * num
+	return multiplication
+	print (mult(number))
+
+#To add two numbers
+def add(a,b):
+
+	addition = a+b
+	return addition
+	print (add(a,b))
+	
+# Data structure
+numberlist = list(input("enter four numbers to be added:"))
+def sumOfListNumbers(a[3]):
+    Sumoflist=item[0]+item[1]+item[3]
+	return Sumorlist
+	print (sumOfListNumbers(numberlist))
+	
